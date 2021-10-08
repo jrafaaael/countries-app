b22327eeee7fe8cdd119661a9dbef1fe0a99d9ce
jrafaaael <jrmd070102@gmail.com>
2021-10-08 18:32:16 -0400

    added toggle to change theme

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,43 @@
+import { useChangeTheme } from "../../hooks/useChangeTheme";
+
 import { Link } from "react-router-dom";
+import { ReactSVG } from "react-svg";
 
 import styles from "./navbar.module.css";
 
 const Navbar = () => {
+  const { isDarkMode, handleChangeTheme } = useChangeTheme();
+
+  const themeText = `${isDarkMode ? 'Light' : 'Dark'} mode`
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.wrapper}>
         <Link to="/">
           <h1 className={styles.title}>Where in the world?</h1>
         </Link>
-        <button>Toggle</button>
+        <button className={styles.toggle} onClick={handleChangeTheme}>
+          {isDarkMode ? (
+            <ReactSVG
+              src={
+                new URL(
+                  "../../assets/icons/sun.svg",
+                  import.meta.url
+                ) as unknown as string
+              }
+            />
+          ) : (
+            <ReactSVG
+              src={
+                new URL(
+                  "../../assets/icons/moon.svg",
+                  import.meta.url
+                ) as unknown as string
+              }
+            />
+          )}
+          {themeText}
+        </button>
       </div>
     </nav>
   );
