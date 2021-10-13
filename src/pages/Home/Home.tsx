c93e7c138397cc63c2997b7ee09bb3d03fff285f
jrafaaael <jrmd070102@gmail.com>
2021-10-13 14:44:33 -0400

    added home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import Filters from "../../components/Filters/Filters";
+import Countries from "../../components/Countries/Countries";
+
+const Home = () => {
+  return (
+    <main>
+      <Filters />
+      <Countries />
+    </main>
+  );
+};
+
+export default Home;
