6f0ee9de6085a245192c9938fd325d93f6c38a45
jrafaaael <jrmd070102@gmail.com>
2021-10-12 21:35:20 -0400

    changed endpoint

--- src/api/Country/getCountries.ts
+++ src/api/Country/getCountries.ts
@@ -3,7 +3,7 @@ import { Country } from "../../types/Country";
 
 export const getCountries = async () => {
   const { data } = await axios.get<Country[]>(
-    "https://restcountries.com/v3.1/all"
+    "https://restcountries.com/v2/all"
   );
   return data;
 };
