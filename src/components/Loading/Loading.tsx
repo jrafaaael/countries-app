189d17479e05b11ed8e65a52c57f1068004e4a01
jrafaaael <jrmd070102@gmail.com>
2021-10-14 19:17:39 -0400

    added loading spinner

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,12 @@
+import { memo } from "react";
+import styles from "./loading.module.css";
+
+const Loading = () => {
+  return (
+    <div className={styles.preloader}>
+      <div className={styles.loader}></div>
+    </div>
+  );
+};
+
+export default memo(Loading);
