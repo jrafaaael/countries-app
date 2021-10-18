fee2310a9d5dfcfeb392e2f1a39ee8d31205e7f2
jrafaaael <jrmd070102@gmail.com>
2021-10-18 18:41:01 -0400

    added localstorage support to the theme

--- src/hooks/useChangeTheme.ts
+++ src/hooks/useChangeTheme.ts
@@ -1,22 +1,36 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "./useMediaQuery";
 
 export const useChangeTheme = () => {
-  const userThemeIsDark = useMediaQuery("(prefers-color-scheme: dark)");
+  const localUserThemeIsDark = useMediaQuery("(prefers-color-scheme: dark)");
+  const establishedThemeIsDark = JSON.parse(
+    localStorage.getItem("isDarkMode")!
+  );
 
-  const [isDarkMode, setIsDarkMode] = useState(userThemeIsDark);
+  const [isDarkMode, setIsDarkMode] = useState<Boolean>(
+    establishedThemeIsDark ?? localUserThemeIsDark
+  );
+  const mountRef = useRef(false);
 
   const handleChangeTheme = () => setIsDarkMode(!isDarkMode);
 
   useEffect(() => {
-    setIsDarkMode(userThemeIsDark)
-  }, [userThemeIsDark]);
+    if (mountRef.current) {
+      setIsDarkMode(localUserThemeIsDark);
+    }
+  }, [localUserThemeIsDark]);
 
   useEffect(() => {
     isDarkMode
       ? document.body.classList.add("dark")
       : document.body.classList.remove("dark");
+
+    localStorage.setItem("isDarkMode", JSON.stringify(isDarkMode));
   }, [isDarkMode]);
+
+  useEffect(() => {
+    mountRef.current = true;
+  }, []);
 
   return {
     isDarkMode,
